6189d9a5d1c84725e22e2e29cede0f45e07a2bb9
Joshua Levin <joshua.levin@deliveroo.co.uk>
2020-04-12 23:37:00 +0100

    tidy up code and add comments

--- Server/main/main.go
+++ Server/main/main.go
@@ -17,53 +17,64 @@ var playerMap = make(map[float64][]byte)
 var mutex sync.Mutex
 
 func main() {
+	// Setting flags and providing defaults
 	l := flag.Int("lag", 0, "integer for how much lag the server should emulate")
-
 	rr := flag.Int("refresh", 5, "integer for number of milliseconds between message broadcasts")
 
 	flag.Parse()
 
+	// Setting the variables
 	lag = *l
 	refreshRate = *rr
 
+	// Gin is the server, melody is for websockets
 	r := gin.Default()
 	m = melody.New()
 
+	// Healthcheck endpoint for testing
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "alive",
 		})
 	})
 
+	// Go to this path to connect to a websocket
 	r.GET("/ws", func(c *gin.Context) {
 		m.HandleRequest(c.Writer, c.Request)
 	})
 
-	m.HandleMessage(func(s *melody.Session, msg []byte) {
+	// Run every time a client sends a websocket message
+	m.HandleMessage(func(s *melody.Session, messageBytes []byte) {
+		// Extracts the JSON message into something Go can interact with
 		var f interface{}
-		err := json.Unmarshal(msg, &f)
+		err := json.Unmarshal(messageBytes, &f)
 		if err != nil {
 			panic(err)
 		}
 
-		u := f.(map[string]interface{})
+		message := f.(map[string]interface{})
 
-		u["duration"] = refreshRate
-		msg, err = json.Marshal(u)
+		// We manually set the time difference between one message and the next
+		message["duration"] = refreshRate
+		messageBytes, err = json.Marshal(message)
 
-		if (u["command"] == "NEW_PLAYER") || (u["command"] == "REFRESH_PLAYER") {
+		// We don't want to buffer new player or refresh commands
+		if (message["command"] == "NEW_PLAYER") || (message["command"] == "REFRESH_PLAYER") {
 			go func() {
+				// Mimic lag, sleep the code for 'lag' milliseconds
 				time.Sleep(time.Duration(lag) * time.Millisecond)
-				m.Broadcast(msg)
+				m.Broadcast(messageBytes)
 			}()
 			return
 		}
-		id := u["player_id"].(float64)
+
+		id := message["player_id"].(float64)
 		if err != nil {
 			panic(err)
 		}
 		mutex.Lock()
-		playerMap[id] = msg
+		// Store the message into a map so we can send it later
+		playerMap[id] = messageBytes
 		mutex.Unlock()
 	})
 
@@ -73,10 +84,13 @@ func main() {
 }
 
 func sendLoop() {
+	// Mimic server refresh rate by sleeping for 'refresh' milliseconds
 	time.Sleep(time.Duration(refreshRate) * time.Millisecond)
+
 	mutex.Lock()
 	for i, msg := range playerMap {
 		go func() {
+			// Mimic lag, sleep the code for 'lag' milliseconds
 			time.Sleep(time.Duration(lag) * time.Millisecond)
 			m.Broadcast(msg)
 		}()
